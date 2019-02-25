d796445d177bf32ceaab64710ae66273e54c1239
Furkan Gözükara <furkangozukara@gmail.com>
2019-02-25 13:41:05 +0300

    week 4 lesson 1

---
+++ program codes/week 4 constructor/week 4/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace week_4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReturValue_Click(object sender, RoutedEventArgs e)
+        {
+            PublicValues myPublicVals = new PublicValues();
+            lblVal.Content = $"Student count for default constructor is: {myPublicVals.irStudentCount}";
+        }
+
+        private void btnReturValue_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            PublicValues myPublicVals = new PublicValues(55);
+            lblVal.Content = $"Student count for integer set constructor is: {myPublicVals.irStudentCount}";
+        }
+
+        private void btnReturValue_Copy1_Click(object sender, RoutedEventArgs e)
+        {
+            PublicValues myPublicVals = new PublicValues(3, 9);
+            lblVal.Content = $"Student count for integer+integer set constructor is: {myPublicVals.irStudentCount}";
+        }
+
+        private void btnReturValue_Copy2_Click(object sender, RoutedEventArgs e)
+        {
+            PublicValues myPublicVals = new PublicValues();
+            myPublicVals.irStudentCount = 100;
+            lblVal.Content = $"Student count my assigned value: {myPublicVals.irStudentCount}";
+
+            lblVal2.Content = $"Student v2 count default value: {myPublicVals.irStudentCount_v2}";
+
+            myPublicVals.irStudentCount_v2 = 10;
+            lblVal2.Content = $"Student v2 count assigned value 10 value: {myPublicVals.irStudentCount_v2}";
+        }
+    }
+}
---
+++ program codes/week 4 constructor/week 4/PublicValues.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_4
+{
+    public class PublicValues
+    {
+        public int irStudentCount = 0;
+
+        private int _irStudentCount_v2 = 0;
+
+        public int irStudentCount_v2
+        {
+            get
+            {
+                return _irStudentCount_v2 * 10;
+            }
+
+            set
+            {
+                _irStudentCount_v2 = value / 2;
+            }
+        }
+
+        //this is empty constrcuter deafult one called when the object of this class is initizaliated 
+        public PublicValues()//signature of this is empty
+        {
+            irStudentCount = 10;
+        }
+
+        public PublicValues(int _irStudentCount)//signature of this is an int
+        {
+            irStudentCount = _irStudentCount;
+        }
+
+        public PublicValues(int _irStudentCount, int _irMultiplier)//signature of this is int+int
+        {
+            irStudentCount = _irStudentCount * _irMultiplier;
+        }
+    }
+}
