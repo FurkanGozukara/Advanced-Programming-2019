8329e6ab1ad651b7034fa9ab20941d19bb381573
Furkan Gözükara <furkangozukara@gmail.com>
2019-03-11 13:27:25 +0300

    sync

---
+++ program codes/week 5 sql/week 5 sql/dbConnection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace week_5_sql
+{
+    public static class dbConnection
+    {
+        public static string srConnectionString = "server=localhost;database=okul;Integrated Security=SSPI;";
+
+        public static DataSet return_data_table(string srQuery, out string Message)
+        {
+            DataSet dSet = new DataSet();
+            Message = "Success";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(srConnectionString))
+                {
+                    connection.Open();
+                    SqlDataAdapter DA = new SqlDataAdapter(srQuery, connection);
+                    DA.Fill(dSet);
+                }
+                return dSet;
+            }
+            catch (Exception E)
+            {
+                Message = "Error happened\r\n" + E.Message.ToString() + "\r\nInner Exception: " +
+                    E?.InnerException?.Message.ToString() + "\r\n" + E?.StackTrace?.ToString();
+                return dSet;
+            }
+        }
+
+
+        public static void update_database(string srQuery, out string Message)
+        {
+            DataSet dSet = new DataSet();
+            Message = "Success";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(srConnectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(srQuery, connection);
+                    Message = " rows affected " + command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception E)
+            {
+                Message = "Error happened\r\n" + E.Message.ToString() + "\r\nInner Exception: " +
+                    E?.InnerException?.Message.ToString() + "\r\n" + E?.StackTrace?.ToString();
+            }
+        }
+    }
+}
