23d286bf788528f124047fb061ba4992397df035
Furkan Gözükara <furkangozukara@gmail.com>
2019-03-11 15:51:39 +0300

    week 6 code

--- program codes/week 5 sql/week 5 sql/MainWindow.xaml.cs
+++ program codes/week 5 sql/week 5 sql/MainWindow.xaml.cs
@@ -50,6 +50,52 @@ namespace week_5_sql
             initStudentsGrid();
         }
 
+        private void datagridStudents_Loaded(object sender, RoutedEventArgs e)
+        {
+            datagridStudents.Columns[0].IsReadOnly = true;
+            datagridStudents.Columns[1].IsReadOnly = true;
+            datagridStudents.Columns[0].CanUserReorder = false;
+            datagridStudents.Columns[1].CanUserReorder = false;
+        }
+
+        private void datagridStudents_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var senderGrid = (DataGrid)sender;
+            DataRowView drw = (DataRowView)senderGrid.SelectedItem;
+            string srClickedStudentId = drw["StudentId"].ToString();
+            var clickedColIndex = senderGrid.CurrentColumn.DisplayIndex;
+
+            //  MessageBox.Show("student id: " + srClickedStudentId + " clicked column: " + clickedColIndex);
+            drw.EndEdit();
+    
+            datagridStudents.CommitEdit();
+
+            if (clickedColIndex == 1)
+                updateDataGridSelectedRow(drw);
+        }
+
+        private void updateDataGridSelectedRow(DataRowView drw)
+        {
+            string srSelectedStudentId = drw["StudentId"].ToString();
+
+            string srUpdateQuery = "update tblOgrenciler set Name=N'{0}',Surname=N'{1}',RegistrationDate=N'{2}',AvgScore={3} where StudentId={4}";
+
+            srUpdateQuery = string.Format(srUpdateQuery,
+                drw["Name"].ToString().Replace("'","''"),
+               drw["Surname"].ToString().Replace("'", "''"),
+               drw["RegistrationDate"].ToString(),
+                 drw["AvgScore"].ToString(),
+                  drw["StudentId"].ToString());
+
+            string srUpdateResult = "";
+
+            dbConnection.update_database(srUpdateQuery, out srUpdateResult);
+
+            MessageBox.Show(srUpdateResult);
+
+            initStudentsGrid();
+        }
+
         private void btnAddStuden_Click(object sender, RoutedEventArgs e)
         {
             if (txtStudenName.Text.Length > 50)
@@ -129,7 +175,6 @@ namespace week_5_sql
             if (dsStudents.Tables.Count > 0)
             {
                 datagridStudents.ItemsSource = dsStudents.Tables[0].DefaultView;
-                datagridStudents.Columns[0].IsReadOnly = true;
             }
         }
     }
