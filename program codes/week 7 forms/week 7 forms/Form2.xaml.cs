c3106d8837661071db7e10f403366b7a245b516e
Furkan Gözükara <furkangozukara@gmail.com>
2019-03-18 13:35:01 +0300

    week 7

---
+++ program codes/week 7 forms/week 7 forms/Form2.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace week_7_forms
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Form2 : Window
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartCounterSingle_Click(object sender, RoutedEventArgs e)
+        {
+            int irCounter = 0;
+            while(true)
+            {
+                irCounter++;
+                lblCounter.Content = irCounter.ToString("N0");
+                System.Threading.Thread.Sleep(1);
+            }
+        }
+
+        private void btnStartMultiThread_Click(object sender, RoutedEventArgs e)
+        {
+            Task t = Task.Factory.StartNew(() =>
+            {
+                CounterFunction();
+            });
+        }
+
+        private void CounterFunction()
+        {
+            int irCounter = 0;
+            while (true)
+            {
+                irCounter++;
+              
+                Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, new Action(() =>
+                {
+                    lblCounter.Content = irCounter.ToString("N0");
+                }));
+
+                System.Threading.Thread.Sleep(1);
+            }
+        }
+    }
+}
---
+++ program codes/week 7 forms/week 7 forms/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace week_7_forms
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenForm2_Click(object sender, RoutedEventArgs e)
+        {
+            Form2 Window = new Form2();
+            Window.Show();
+        }
+    }
+}
