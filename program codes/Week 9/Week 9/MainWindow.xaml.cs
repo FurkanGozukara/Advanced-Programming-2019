e435822916a961a97573d527d3d9de60815eed19
Furkan Gözükara <furkangozukara@gmail.com>
2019-04-01 14:20:09 +0300

    week 9 update

---
+++ program codes/Week 9/Week 9/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Week_9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void refreshListBox_Click(object sender, RoutedEventArgs e)
+        {
+            string srResult = "";
+            lstRecords.Items.Clear();
+            foreach (DataRow drw in dbConnection.return_data_set("select * from tblLessonStudents order by LessonId asc , StudentName asc",
+                out srResult).Tables[0].Rows)
+            {
+                lstRecords.Items.Add("Lesson ID: " + drw["LessonId"].ToString() + "\t\t" + drw["StudentName"].ToString());
+            }
+
+            //DataSet ds = dbConnection.return_data_set("select * from tblLessonStudents order by LessonId asc , StudentName asc",
+            //     out srResult);
+
+            //foreach (DataRow drw in ds.Tables[0].Rows)
+            //{
+
+            //}
+        }
+
+        private void btnAddRecord_Click(object sender, RoutedEventArgs e)
+        {
+            int irLessonId = 0;
+
+            Int32.TryParse(txtStudentId.Text, out irLessonId);
+
+            if(irLessonId<1)
+            {
+                MessageBox.Show("incorrect lesson id");
+                return;
+            }
+
+            dbConnection.cmd_update_DB
+        }
+    }
+}
---
+++ program codes/Week 9/Week 9/dbConnection.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+public static class dbConnection
+{
+    public static string srConnectionString = "server=localhost;database=okul;Integrated Security=SSPI;";
+    //public static string srConnectionString = "server=DESKTOP-ULH4M26;database=okul;Integrated Security=SSPI;";
+
+    public static DataSet return_data_set(string srQuery, out string Message)
+    {
+        DataSet dSet = new DataSet();
+        Message = "Success";
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(srConnectionString))
+            {
+                connection.Open();
+                SqlDataAdapter DA = new SqlDataAdapter(srQuery, connection);
+                DA.Fill(dSet);
+            }
+            return dSet;
+        }
+        catch (Exception E)
+        {
+            Message = "Error happened\r\n" + E.Message.ToString() + "\r\nInner Exception: " +
+                E?.InnerException?.Message.ToString() + "\r\n" + E?.StackTrace?.ToString();
+            return dSet;
+        }
+    }
+
+    public static void update_database(string srQuery, out string Message)
+    {
+        DataSet dSet = new DataSet();
+        Message = "Success";
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(srConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(srQuery, connection);
+                Message = " rows affected " + command.ExecuteNonQuery();
+            }
+        }
+        catch (Exception E)
+        {
+            Message = "Error happened\r\n" + E.Message.ToString() + "\r\nInner Exception: " +
+                E?.InnerException?.Message.ToString() + "\r\n" + E?.StackTrace?.ToString();
+        }
+    }
+
+    public class cmdParameterType  {
+
+        public cmdParameterType( string _parameterName,object _objParam)
+        {
+            parameterName = _parameterName;
+            objParam = _objParam;
+        }
+
+        public cmdParameterType()
+        {
+
+        }
+
+        public string parameterName = "";
+        public object objParam;
+    }
+
+    public static DataTable cmd_Select_DB(string srQuery, List<cmdParameterType> lstParameters)
+    {
+        DataTable dtTable = new DataTable();
+
+        using (SqlConnection connection = new SqlConnection(dbConnection.srConnectionString))
+        using (SqlCommand command = new SqlCommand(srQuery, connection))
+        {
+            foreach (var vrPerParameter in lstParameters)
+            {
+                command.Parameters.AddWithValue(vrPerParameter.parameterName, vrPerParameter.objParam);
+            }
+
+            connection.Open();
+
+            dtTable.Load(command.ExecuteReader());
+        }
+
+        return dtTable;
+    }
+
+    public static int cmd_update_DB(string srQuery, List<cmdParameterType> lstParameters)
+    {
+        using (SqlConnection connection = new SqlConnection(dbConnection.srConnectionString))
+        using (SqlCommand command = new SqlCommand(srQuery, connection))
+        {
+            foreach (var vrPerParameter in lstParameters)
+            {
+                command.Parameters.AddWithValue(vrPerParameter.parameterName, vrPerParameter.objParam);
+            }
+
+            connection.Open();
+
+            return command.ExecuteNonQuery();
+        }
+    }
+
+}
+
