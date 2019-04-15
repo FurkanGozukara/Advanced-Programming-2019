b7038f82152fbece3a2b8ceaf832cdd2405ea2f6
Furkan Gözükara <furkangozukara@gmail.com>
2019-04-15 14:46:17 +0300

    sync

--- program codes/week 11 web crawler/week 11 web crawler/Cryptology.cs
+++ program codes/week 11 web crawler/week 11 web crawler/Cryptology.cs
@@ -6,35 +6,34 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace week_11
+
+public static class Cryptology
 {
-    public static class Cryptology
+    public static string ComputeSha256HashFromString(string rawData)
     {
-        static string ComputeSha256HashFromString(string rawData)
+        // Create a SHA256   
+        using (SHA256 sha256Hash = SHA256.Create())
         {
-            // Create a SHA256   
-            using (SHA256 sha256Hash = SHA256.Create())
-            {
-                // ComputeHash - returns byte array  
-                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+            // ComputeHash - returns byte array  
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
 
-                // Convert byte array to a string   
-                StringBuilder builder = new StringBuilder();
-                for (int i = 0; i < bytes.Length; i++)
-                {
-                    builder.Append(bytes[i].ToString("x2"));
-                }
-                return builder.ToString();
+            // Convert byte array to a string   
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
             }
+            return builder.ToString();
         }
+    }
 
-        public static string ComputeSha256HashFromFile(string filePath)
+    public static string ComputeSha256HashFromFile(string filePath)
+    {
+        using (SHA256 SHA256 = SHA256Managed.Create())
         {
-            using (SHA256 SHA256 = SHA256Managed.Create())
-            {
-                using (FileStream fileStream = File.OpenRead(filePath))
-                    return Convert.ToBase64String(SHA256.ComputeHash(fileStream));
-            }
+            using (FileStream fileStream = File.OpenRead(filePath))
+                return Convert.ToBase64String(SHA256.ComputeHash(fileStream));
         }
     }
 }
+
