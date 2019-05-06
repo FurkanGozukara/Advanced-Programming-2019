372dd16ce54bfb7bf818a5066706271d888ab3d0
Furkan Gözükara <furkangozukara@gmail.com>
2019-05-06 13:52:19 +0300

    week 14

---
+++ program codes/week 14 interfaces/week 14 interfaces/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_14_interfaces
+{
+
+    class Program
+    {
+        interface Cars
+        {
+            int getCarWeight();
+            string getCarColor();
+            string getCarBrand();
+        }
+
+        public class Bus : Cars
+        {
+            public int getCarWeight()
+            {
+                return 0;
+            }
+            public string getCarColor()
+            {
+                return "Red";
+            }
+            public string getCarBrand()
+            {
+                return "Tesla";
+            }
+        }
+
+        public class School
+        {
+            private static string SchoolId = "1";
+            protected static string SchoolName = "";
+            public static int StudentCount = 0;
+
+            public string returnSchoolId()
+            {
+                return SchoolId;
+            }
+
+            public class SubSchool
+            {
+                public string returnShoolId()
+                {
+                    return SchoolId;
+                }
+            }
+        }
+
+        public class University : School
+        {
+            public string returnSchoolName()
+            {
+                return SchoolName;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            School mySchool = new School();
+            School.StudentCount = 25;
+
+            School mySchool2 = new School();
+            School.StudentCount = 25;
+
+            University myUni = new University();
+            myUni.returnSchoolName();
+        }
+    }
+}
